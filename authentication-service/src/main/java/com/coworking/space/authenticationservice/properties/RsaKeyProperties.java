5fb681347a5c226c0cd0b7da64fda49c5f87c238
mkhalez <mikhalko0606@mail.ru>
2026-08-01 14:49:38 +0300

    restriction on creating the RsaKeyProperties bean

--- authentication-service/src/main/java/com/coworking/space/authenticationservice/properties/RsaKeyProperties.java
+++ authentication-service/src/main/java/com/coworking/space/authenticationservice/properties/RsaKeyProperties.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -13,6 +14,7 @@ import java.security.interfaces.RSAPublicKey;
 @AllArgsConstructor
 @Getter
 @Setter
+@Profile("!test")
 public class RsaKeyProperties {
     private RSAPublicKey publicKey;
 
